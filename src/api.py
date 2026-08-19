7ff907d945933241403ac6cb4b53e65db77969f1
Ahmed <newahmed16772022@gmail.com>
2026-08-19 12:02:33 +0300

    Added lazy loading support to api server

--- src/api.py
+++ src/api.py
@@ -1,18 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from src.model import RideDurationModel
 from src.schemas import PredictRequest, PredictResponse
 from configs.config import get_settings
+from functools import lru_cache
+
 
 settings = get_settings()
 
+@lru_cache
+def get_model() -> RideDurationModel:
+    RideDurationModel(model_path=settings.model_path_resolved)
+
 app = FastAPI(title="Ride Duration service")
 
-model = RideDurationModel(model_path=settings.model_path_resolved)
 
 @app.get("/health")
 def health():
     return {"status": "alive and kicking"}
 
 @app.post("/predict")
-def predict(request: PredictRequest):
+def predict(request: PredictRequest, 
+            model: RideDurationModel = Depends(get_model)):
     return PredictResponse(duration=model.predict([request.distance_km, request.passengers]))
